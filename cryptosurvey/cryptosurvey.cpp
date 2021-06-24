1e016b795b066ccc99b463f19e322ce6e0845933
SOURABHMISHRA5221 <Sourabh19278@iiitd.ac.in>
2021-06-24 13:56:52 +0530

    finalized cryptosurvey main smart contract

--- cryptosurvey/cryptosurvey.cpp
+++ cryptosurvey/cryptosurvey.cpp
@@ -4,7 +4,7 @@
 using namespace eosio;
 using namespace std;
 
-#define ON_TRANSFER
+#define ON_TRANSFER [[eosio::on_notify("eosio.token::transfer")]]
 
 CONTRACT cryptosurvey : public contract{
 	public:
@@ -14,18 +14,65 @@ CONTRACT cryptosurvey : public contract{
            name code,
            datastream<const char *> ds
          ):contract(reciver,code,ds),
-           myToken("ORE",4){}
+           myToken("ORE",4)
+           {}
 
         using contract::contract;
 
-        TABLE users{
+        TABLE credscrore{
            name user;
+           int score;
            auto primary_key() const {return user.value;}
         };
+
+        typedef multi_index<name("credscore"),credscrore> credscrores;
+
+        ACTION insertscore(name user,int score){
+           require_auth(name("cryptosurvey"));
+           addScore(user,score);
+        }
+
+        ACTION deleteuser(name user){
+           require_auth(name("cryptosurvey"));
+           delUser(user);
+        }
+
        
-        typedef multi_index<name("users"),users> userTable;
+
+
         
    private:
       const symbol myToken;
+      
+      
+      void addScore(name user,int score){
+         credscrores _credscore(get_self(),get_self().value);
+         auto itr = _credscore.find(user.value);
+         if ( itr == _credscore.end()){
+            addNewUser(user,score);
+         }
+         else{
+            _credscore.modify(itr,get_self(),[&](auto& olduser){
+                      olduser.score += score;
+            });
+         }
+      }
+
+      void addNewUser(name user,int value){
+         credscrores _credscore(get_self(),get_self().value);
+         _credscore.emplace(get_self(),[&](auto& newUser){
+              newUser.user = user;
+              newUser.score = value;
+         });
+      }
+
+      void delUser(name user){
+         credscrores _credscore(get_self(),get_self().value);
+         auto itr = _credscore.find(user.value);
+         check(itr != _credscore.end(),"USER DOES'T EXIST");
+         _credscore.erase(itr);
+      }
+
+      
 
 };
